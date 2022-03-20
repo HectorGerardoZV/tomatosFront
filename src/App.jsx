6d88a6e05d484a266959d310aad088ebefc1e99c
DianaLauraMN <dianaalauramn@gmail.com>
2022-03-19 20:57:35 -0700

    The panelOrders was created and added in the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import PanelOrders from "./pages/panelorders/PanelOrders"
 import Principal from "./pages/principal/Principal"
 
 function App() {
@@ -7,10 +8,9 @@ function App() {
   return (
     <Router>
       <Routes>
-
         <Route path="/cajero" element={<Principal />}>
+          <Route path="panelorders" element={<PanelOrders/>}/>
         </Route>
-
       </Routes>
     </Router>
   )
